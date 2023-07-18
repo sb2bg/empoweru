0c4312577f944340e472cda3b31741e31f18c1b7
sb2bg <sullivanbognar@gmail.com>
2023-07-18 01:10:33 -0500

    Add sign up util

--- lib/supabase/auth/email.dart
+++ lib/supabase/auth/email.dart
@@ -3,3 +3,7 @@ import '../../utils/constants.dart';
 Future<void> signInWithEmail(String email, String password) async {
   await supabase.auth.signInWithPassword(email: email, password: password);
 }
+
+Future<void> signUpWithEmail(String email, String password) async {
+  await supabase.auth.signUp(email: email, password: password);
+}
