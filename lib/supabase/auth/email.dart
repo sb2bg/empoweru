a9990d7189af4524e4b8a7bb746cd8b291f04f27
sb2bg <sullivanbognar@gmail.com>
2023-07-19 01:54:20 -0500

    Add name on sign up

--- lib/supabase/auth/email.dart
+++ lib/supabase/auth/email.dart
@@ -4,6 +4,7 @@ Future<void> signInWithEmail(String email, String password) async {
   await supabase.auth.signInWithPassword(email: email, password: password);
 }
 
-Future<void> signUpWithEmail(String email, String password) async {
-  await supabase.auth.signUp(email: email, password: password);
+Future<void> signUpWithEmail(String email, String password, String name) async {
+  await supabase.auth
+      .signUp(email: email, password: password, data: {'name': name});
 }
