9993dbff6ff289569a291dd7ff1902bcc86daefb
sb2bg <sullivanbognar@gmail.com>
2023-07-25 12:19:56 -0500

    Add email sign up page

---
+++ lib/pages/email_sign_up_page.dart
@@ -0,0 +1,136 @@
+import 'package:age_sync/supabase/auth/email.dart';
+import 'package:age_sync/utils/constants.dart';
+import 'package:age_sync/widgets/password_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'email_log_in_page.dart';
+
+class EmailSignUpPage extends StatefulWidget {
+  static const routeName = '/email-sign-up';
+  const EmailSignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _EmailSignUpPageState();
+}
+
+class _EmailSignUpPageState extends State<EmailSignUpPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _nameController = TextEditingController();
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign up'),
+        leading: const BackButton(),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(
+                hintText: 'Full Name',
+                prefixIcon: Icon(Icons.person),
+              ),
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                hintText: 'Email',
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+            const SizedBox(height: 8),
+            PasswordTextField(controller: _passwordController),
+            const SizedBox(height: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        context.pushNamed('todo');
+                      },
+                      child: const Text('Forgot Password?'),
+                    ),
+                    ElevatedButton(
+                      onPressed: _isLoading ? null : () => _signUp(),
+                      child: const Text('Sign Up'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Already have an account?"),
+                TextButton(
+                  onPressed: _isLoading
+                      ? null
+                      : () => context.popOrPushNamed(EmailLogInPage.routeName),
+                  child: const Text('Sign In'),
+                )
+              ],
+            ),
+            SizedBox(height: MediaQuery.of(context).padding.bottom),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // TODO: deduplicate this code from lib/pages/login_page.dart
+  _signUp() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      if (!validateEmail(_emailController.text)) {
+        context.showErrorSnackBar(message: 'Invalid email address');
+      }
+
+      await signUpWithEmail(
+          email: _emailController.text,
+          password: _passwordController.text,
+          name: _nameController.text,
+          elder: true);
+    } on AuthException catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(error.message),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      ));
+    } on PlatformException catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(error.message!),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      ));
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: const Text('Unexpected error occurred'),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      ));
+    } finally {
+      if (mounted) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+  }
+
+  bool validateEmail(String email) => RegExp(
+          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+      .hasMatch(email);
+}
