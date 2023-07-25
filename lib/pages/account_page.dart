22ba97ba30d43810ee94fe1a5f41e11e5107dd7f
sb2bg <sullivanbognar@gmail.com>
2023-07-25 13:26:10 -0500

    Convert account page to use LoadingState

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,6 +1,6 @@
+import 'package:age_sync/utils/loading_state.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../utils/constants.dart';
 import '../utils/profile.dart';
@@ -16,30 +16,19 @@ class AccountPage extends StatefulWidget {
   State<AccountPage> createState() => _AccountPageState();
 }
 
-class _AccountPageState extends State<AccountPage> {
-  late Profile _profile;
-  final _loading = ValueNotifier(true);
+class _AccountPageState extends LoadingState<AccountPage> {
+  late final Profile _profile;
 
-  Future<void> _getProfile() async {
-    try {
+  @override
+  Future<void> onInit() async {
+    await context.tryDatabaseAsync(() async {
       final userId = supabase.auth.currentUser!.id;
-
       final profile = await Profile.fromUuid(userId);
 
       setState(() {
         _profile = profile;
       });
-    } on PostgrestException catch (error) {
-      context.showErrorSnackBar(message: error.message);
-    } catch (error) {
-      if (mounted) {
-        context.showErrorSnackBar(message: error.toString());
-      }
-    } finally {
-      if (mounted) {
-        _loading.value = false;
-      }
-    }
+    });
   }
 
   _signOut() {
@@ -48,64 +37,51 @@ class _AccountPageState extends State<AccountPage> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    _getProfile();
-  }
+  AppBar? get constAppBar => AppBar(
+        title: const Text('Account'),
+      );
 
   @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-        valueListenable: _loading,
-        builder: (context, value, child) {
-          if (value) {
-            return preloader;
-          }
-
-          return Scaffold(
-            appBar: AppBar(title: const Text('Profile')),
-            body: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  ListTile(
-                      leading: CircleAvatar(
-                        radius: 20,
-                        backgroundImage:
-                            CachedNetworkImageProvider(_profile.avatarUrl),
-                      ),
-                      title: Text(_profile.name),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.edit),
-                        onPressed: () => print('TODO'),
-                      )),
-                  const Divider(),
-                  ListTile(
-                      leading: const Icon(Icons.email_outlined),
-                      title: const Text('Change email'),
-                      onTap: () => print('TODO')),
-                  ListTile(
-                      leading: const Icon(Icons.lock_outline),
-                      title: const Text('Change password'),
-                      onTap: () => print('TODO')),
-                  ListTile(
-                    leading: const Icon(Icons.logout),
-                    title: const Text('Sign Out'),
-                    onTap: _signOut,
-                  ),
-                  const Divider(),
-                  ListTile(
-                    leading: const Icon(
-                      Icons.delete_outline,
-                    ),
-                    title: const Text('Delete account'),
-                    onTap: () => Navigator.of(context)
-                        .pushNamed(ViewMessagesPage.routeName),
-                  ),
-                ],
+  Widget buildLoaded(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          ListTile(
+              leading: CircleAvatar(
+                radius: 20,
+                backgroundImage: CachedNetworkImageProvider(_profile.avatarUrl),
               ),
+              title: Text(_profile.name),
+              trailing: IconButton(
+                icon: const Icon(Icons.edit),
+                onPressed: () => print('TODO'),
+              )),
+          const Divider(),
+          ListTile(
+              leading: const Icon(Icons.email_outlined),
+              title: const Text('Change email'),
+              onTap: () => print('TODO')),
+          ListTile(
+              leading: const Icon(Icons.lock_outline),
+              title: const Text('Change password'),
+              onTap: () => print('TODO')),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Sign Out'),
+            onTap: _signOut,
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(
+              Icons.delete_outline,
             ),
-          );
-        });
+            title: const Text('Delete account'),
+            onTap: () =>
+                Navigator.of(context).pushNamed(ViewMessagesPage.routeName),
+          ),
+        ],
+      ),
+    );
   }
 }
