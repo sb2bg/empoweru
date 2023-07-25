16e78ffda8bcbda8a6108cf05f8fc09bf29687d9
sb2bg <sullivanbognar@gmail.com>
2023-07-25 12:18:29 -0500

    Add new routes and argument passing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:age_sync/pages/account_page.dart';
-import 'package:age_sync/pages/chat_page.dart';
+import 'package:age_sync/pages/chat/chat_page.dart';
+import 'package:age_sync/pages/chat/new_chat_page.dart';
+import 'package:age_sync/pages/chat/view_messages.dart';
 import 'package:age_sync/pages/email_log_in_page.dart';
-import 'package:age_sync/pages/login_page.dart';
+import 'package:age_sync/pages/email_sign_up_page.dart';
+import 'package:age_sync/pages/log_in_page.dart';
 import 'package:age_sync/pages/splash.dart';
-import 'package:age_sync/pages/view_messages.dart';
 import 'package:age_sync/utils/constants.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/material.dart';
@@ -27,37 +29,37 @@ class MyApp extends StatelessWidget {
     debugPaintSizeEnabled = false;
 
     return MaterialApp(
-      title: 'Home',
+        title: 'Home',
         navigatorObservers: [navObserver],
-      themeMode: ThemeMode.dark,
-      darkTheme: ThemeData(
-        colorScheme: const ColorScheme.dark(),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.grey[900],
-          foregroundColor: Colors.white,
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.black,
-            shape: RoundedRectangleBorder(
+        themeMode: ThemeMode.dark,
+        darkTheme: ThemeData(
+          colorScheme: const ColorScheme.dark(),
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.grey[900],
+            foregroundColor: Colors.white,
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+              shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10),
+              ),
             ),
           ),
-        ),
-        snackBarTheme: const SnackBarThemeData(
-          behavior: SnackBarBehavior.floating,
-          elevation: 0,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(12))),
-        ),
-        dividerTheme: DividerThemeData(
-          color: Colors.grey[900],
-        ),
-        listTileTheme: ListTileThemeData(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
+          snackBarTheme: const SnackBarThemeData(
+            behavior: SnackBarBehavior.floating,
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(12))),
           ),
+          dividerTheme: DividerThemeData(
+            color: Colors.grey[900],
+          ),
+          listTileTheme: ListTileThemeData(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
           ),
           inputDecorationTheme: InputDecorationTheme(
             border: OutlineInputBorder(
@@ -70,18 +72,29 @@ class MyApp extends StatelessWidget {
             filled: true,
             fillColor: Colors.grey[900],
             contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+          ),
+          useMaterial3: true,
         ),
-        useMaterial3: true,
-      ),
-      initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        SplashPage.routeName: (_) => const SplashPage(),
-        LoginPage.routeName: (_) => const LoginPage(),
-        AccountPage.routeName: (_) => const AccountPage(),
-        ViewMessagesPage.routeName: (_) => const ViewMessagesPage(),
-        ChatPage.routeName: (_) => const ChatPage(),
-        EmailLogInPage.routeName: (_) => const EmailLogInPage(),
-      },
-    );
+        initialRoute: '/',
+        onGenerateRoute: (RouteSettings settings) {
+          var routes = <String, WidgetBuilder>{
+            SplashPage.routeName: (_) => const SplashPage(),
+            LogInPage.logInRouteName: (_) =>
+                const LogInPage(type: LogInType.signIn),
+            LogInPage.signUpRouteName: (_) =>
+                const LogInPage(type: LogInType.signUp),
+            AccountPage.routeName: (_) => const AccountPage(),
+            ViewMessagesPage.routeName: (_) => const ViewMessagesPage(),
+            ChatPage.routeName: (_) =>
+                ChatPage(roomId: settings.arguments as String),
+            EmailLogInPage.routeName: (_) => const EmailLogInPage(),
+            EmailSignUpPage.routeName: (_) => const EmailSignUpPage(),
+            NewChatPage.routeName: (_) => const NewChatPage(),
+          };
+
+          WidgetBuilder builder = routes[settings.name]!;
+          return MaterialPageRoute(
+              builder: (ctx) => builder(ctx), settings: settings);
+        });
   }
 }
