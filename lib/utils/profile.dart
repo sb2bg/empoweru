a3700c465504a3d0a6aef147a7b586d0f8ae2b26
sb2bg <sullivanbognar@gmail.com>
2023-07-25 12:22:34 -0500

    Add fromUuid method

--- lib/utils/profile.dart
+++ lib/utils/profile.dart
@@ -1,3 +1,5 @@
+import 'constants.dart';
+
 class Profile {
   Profile({
     required this.id,
@@ -13,4 +15,9 @@ class Profile {
       : id = map['id'],
         name = map['name'] ?? '', // TODO: remove null check, enforce name in db
         avatarUrl = map['avatar_url'];
+
+  static Future<Profile> fromUuid(String uuid) async {
+    return Profile.fromMap(
+        await supabase.from('profiles').select().eq('id', uuid).single());
+  }
 }
