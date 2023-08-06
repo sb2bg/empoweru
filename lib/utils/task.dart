62500f8ffcb6d840bcd95a86f133173c38cd5edc
sb2bg <sullivanbognar@gmail.com>
2023-08-06 04:27:33 -0500

    Task model

---
+++ lib/utils/task.dart
@@ -0,0 +1,53 @@
+import 'constants.dart';
+
+class Task {
+  Task({
+    required this.id,
+    required this.name,
+    required this.details,
+    required this.deadline,
+    required this.completed,
+  });
+
+  final String id;
+  final String name;
+  final String details;
+  final DateTime deadline;
+  bool completed;
+
+  Task.fromMap(Map<String, dynamic> map)
+      : id = map['id'] as String,
+        name = map['name'] as String,
+        details = map['details'] as String,
+        deadline = DateTime.parse(map['deadline'] as String),
+        completed = map['completed'] as bool;
+
+  static Future<Task> fromId(String uuid) async {
+    return Task.fromMap(
+        await supabase.from('tasks').select().eq('id', uuid).single());
+  }
+
+  static Future<List<Task>> assignedFromProfileId(String uuid) async {
+    final List<dynamic> tasks =
+        await supabase.from('task_assignees').select().eq('assignee_id', uuid);
+
+    return await Future.wait(
+        tasks.map((map) => Task.fromId(map['task_id'] as String)));
+  }
+
+  static Future<List<Task>> createdFromProfileId(String uuid) async {
+    final List<dynamic> tasks =
+        await supabase.from('tasks').select().eq('owner_id', uuid);
+
+    return await Future.wait(
+        tasks.map((map) => Task.fromId(map['id'] as String)));
+  }
+
+  toggleCompleted() async {
+    completed = !completed;
+
+    await supabase.from('tasks').update({
+      'completed': completed,
+    }).eq('id', id);
+  }
+}
