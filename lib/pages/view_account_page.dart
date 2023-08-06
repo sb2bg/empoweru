14727dc68209dcf46cc9bd72fa6a64510be32a5d
sb2bg <sullivanbognar@gmail.com>
2023-08-06 04:25:50 -0500

    Basic account view

--- lib/pages/view_account_page.dart
+++ lib/pages/view_account_page.dart
@@ -1,8 +1,11 @@
+import 'package:age_sync/utils/constants.dart';
 import 'package:age_sync/utils/loading_state.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../utils/profile.dart';
+import 'account_page.dart';
+import 'chat/chat_page.dart';
 
 class ViewAccountPage extends StatefulWidget {
   static const routeName = '/view-account';
@@ -17,13 +20,16 @@ class ViewAccountPage extends StatefulWidget {
 
 class _ViewAccountPageState extends LoadingState<ViewAccountPage> {
   late final Profile _profile;
+  late final bool _isFriend;
 
   @override
   Future<void> onInit() async {
     final profile = await Profile.fromId(widget.userId);
+    final friend = await profile.isFriend(profile.id);
 
     setState(() {
       _profile = profile;
+      _isFriend = friend;
     });
   }
 
@@ -32,16 +38,50 @@ class _ViewAccountPageState extends LoadingState<ViewAccountPage> {
         title: Text(_profile.name),
         actions: [
           IconButton(
-            icon: const Icon(Icons.report),
-            onPressed: () => print('TODO'),
-          )
+              icon: const Icon(Icons.report_outlined),
+              onPressed: () {
+                print('TODO');
+                showReportThankYouDialog(context);
+              })
         ],
       );
 
   @override
   Widget buildLoaded(BuildContext context) {
-    return Center(
-      child: Image(image: CachedNetworkImageProvider(_profile.avatarUrl)),
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0),
+      child: Center(
+        child: Column(
+          children: [
+            CircleAvatar(
+              backgroundImage: CachedNetworkImageProvider(_profile.avatarUrl),
+              radius: 50,
+            ),
+            const Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton.icon(
+                    icon: Icon(_isFriend
+                        ? Icons.remove_circle_outline
+                        : Icons.person_add_alt_1_outlined),
+                    label: Text(
+                        _isFriend ? 'Remove Friend' : 'Send Friend Request'),
+                    onPressed: () {}),
+                _isFriend
+                    ? TextButton.icon(
+                        icon: const Icon(Icons.chat_outlined),
+                        label: const Text('Send Message'),
+                        onPressed: () {
+                          context.pushNamed(ChatPage.routeName,
+                              arguments: _profile.id);
+                        })
+                    : Container(),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
