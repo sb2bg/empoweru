516ff5e7304540d7a8cfa6ca6a37e63fcc30d569
sb2bg <sullivanbognar@gmail.com>
2023-08-06 04:25:13 -0500

    Create friend page

---
+++ lib/pages/friend_page.dart
@@ -0,0 +1,68 @@
+import 'package:age_sync/pages/view_account_page.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/constants.dart';
+import '../utils/loading_state.dart';
+import '../utils/profile.dart';
+import 'chat/chat_page.dart';
+
+class FriendPage extends StatefulWidget {
+  static const routeName = '/friends';
+
+  const FriendPage({super.key});
+
+  @override
+  State<FriendPage> createState() => _FriendPageState();
+}
+
+class _FriendPageState extends LoadingState<FriendPage> {
+  late final List<Profile> _friends;
+
+  @override
+  onInit() async {
+    final profile = await supabase.getCurrentUser();
+    final friends = await profile.getFriends();
+
+    setState(() {
+      _friends = friends;
+    });
+  }
+
+  @override
+  AppBar? get constAppBar => AppBar(
+        title: const Text('Friends'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              print('TODO');
+            },
+          )
+        ],
+      );
+
+  @override
+  Widget buildLoaded(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: ListView.separated(
+        itemBuilder: (context, index) {
+          final friend = _friends[index];
+
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(friend.avatarUrl),
+            ),
+            title: Text(friend.name),
+            onTap: () {
+              context.pushNamed(ViewAccountPage.routeName,
+                  arguments: friend.id);
+            },
+          );
+        },
+        separatorBuilder: (context, index) => const Divider(),
+        itemCount: _friends.length,
+      ),
+    );
+  }
+}
