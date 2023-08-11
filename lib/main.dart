340f1215699709ae8e6fa38cd6e3b344c20dd2cc
sb2bg <sullivanbognar@gmail.com>
2023-08-10 22:43:35 -0500

    Use persistent nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:age_sync/pages/chat/new_chat_page.dart';
 import 'package:age_sync/pages/chat/view_messages.dart';
 import 'package:age_sync/pages/email_log_in_page.dart';
 import 'package:age_sync/pages/email_sign_up_page.dart';
+import 'package:age_sync/pages/error_page.dart';
 import 'package:age_sync/pages/friend_page.dart';
 import 'package:age_sync/pages/log_in_page.dart';
 import 'package:age_sync/pages/splash.dart';
@@ -12,6 +13,7 @@ import 'package:age_sync/pages/view_account_page.dart';
 import 'package:age_sync/utils/constants.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
@@ -24,8 +26,56 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+WidgetBuilder getRoute(String routeName, RouteSettings settings) {
+  return <String, WidgetBuilder>{
+        SplashPage.routeName: (_) => const SplashPage(),
+        LogInPage.logInRouteName: (_) =>
+            const LogInPage(type: LogInType.signIn),
+        LogInPage.signUpRouteName: (_) =>
+            const LogInPage(type: LogInType.signUp),
+        AccountPage.routeName: (_) => const AccountPage(),
+        ViewAccountPage.routeName: (_) {
+          final userId = settings.arguments as String;
+
+          if (userId == supabase.userId) {
+            return const AccountPage();
+          }
+
+          return ViewAccountPage(userId: userId);
+        },
+        ViewMessagesPage.routeName: (_) => const ViewMessagesPage(),
+        ChatPage.routeName: (_) =>
+            ChatPage(otherId: settings.arguments as String),
+        EmailLogInPage.routeName: (_) => const EmailLogInPage(),
+        EmailSignUpPage.routeName: (_) => const EmailSignUpPage(),
+        NewChatPage.routeName: (_) => const NewChatPage(),
+        TaskPage.routeName: (_) => const TaskPage(),
+        FriendPage.routeName: (_) => const FriendPage(),
+      }[routeName] ??
+      (_) => const ErrorPage(error: 'Route not found');
+}
+
+MaterialPageRoute routeGenerator(RouteSettings settings) {
+  WidgetBuilder builder = getRoute(settings.name!, settings);
+
+  return MaterialPageRoute(builder: (ctx) => builder(ctx), settings: settings);
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final PersistentTabController _controller;
+
+  @override
+  initState() {
+    super.initState();
+    _controller = PersistentTabController(initialIndex: 0);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,82 +83,48 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
         title: 'Home',
+        debugShowCheckedModeBanner: false,
         navigatorObservers: [navObserver],
         themeMode: ThemeMode.dark,
-        darkTheme: ThemeData(
-          colorScheme: const ColorScheme.dark(),
-          appBarTheme: AppBarTheme(
-            backgroundColor: Colors.grey[900],
-            foregroundColor: Colors.white,
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.white,
-              foregroundColor: Colors.black,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-          ),
-          snackBarTheme: const SnackBarThemeData(
-            behavior: SnackBarBehavior.floating,
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(12))),
-          ),
-          dividerTheme: DividerThemeData(
-            color: Colors.grey[900],
-          ),
-          listTileTheme: ListTileThemeData(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          inputDecorationTheme: InputDecorationTheme(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide.none,
-            ),
-            prefixIconColor: Colors.grey[600],
-            suffixIconColor: Colors.grey[600],
-            hintStyle: TextStyle(color: Colors.grey[600]),
-            filled: true,
-            fillColor: Colors.grey[900],
-            contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-          ),
-          dividerColor: Colors.grey[900],
-          useMaterial3: true,
-        ),
+        darkTheme: themeData,
         initialRoute: '/',
-        onGenerateRoute: (RouteSettings settings) {
-          var routes = <String, WidgetBuilder>{
-            SplashPage.routeName: (_) => const SplashPage(),
-            LogInPage.logInRouteName: (_) =>
-                const LogInPage(type: LogInType.signIn),
-            LogInPage.signUpRouteName: (_) =>
-                const LogInPage(type: LogInType.signUp),
-            AccountPage.routeName: (_) => const AccountPage(),
-            ViewAccountPage.routeName: (_) {
-              final userId = settings.arguments as String;
-              if (userId == supabase.userId) {
-                return const AccountPage();
-              }
+        home: PersistentTabView(
+          context,
+          controller: _controller,
+          screens: generateScreens(),
+          items: generateNavBarItems(),
+          backgroundColor: Colors.grey[900]!,
+          navBarStyle: NavBarStyle.style3,
+        ));
+  }
 
-              return ViewAccountPage(userId: userId);
-            },
-            ViewMessagesPage.routeName: (_) => const ViewMessagesPage(),
-            ChatPage.routeName: (_) =>
-                ChatPage(otherId: settings.arguments as String),
-            EmailLogInPage.routeName: (_) => const EmailLogInPage(),
-            EmailSignUpPage.routeName: (_) => const EmailSignUpPage(),
-            NewChatPage.routeName: (_) => const NewChatPage(),
-            TaskPage.routeName: (_) => const TaskPage(),
-            FriendPage.routeName: (_) => const FriendPage(),
-          };
+  PersistentBottomNavBarItem _generateNavBarItem(
+      {required String title, required IconData icon}) {
+    return PersistentBottomNavBarItem(
+      icon: Icon(icon),
+      title: title,
+      activeColorPrimary: themeData.colorScheme.primary,
+      inactiveColorPrimary: Colors.grey,
+    );
+  }
 
-          WidgetBuilder builder = routes[settings.name]!;
-          return MaterialPageRoute(
-              builder: (ctx) => builder(ctx), settings: settings);
-        });
+  List<PersistentBottomNavBarItem> generateNavBarItems() {
+    return [
+      _generateNavBarItem(title: 'Messages', icon: Icons.message),
+      _generateNavBarItem(title: 'Friends', icon: Icons.people),
+      _generateNavBarItem(title: 'Tasks', icon: Icons.task),
+      _generateNavBarItem(title: 'Account', icon: Icons.account_circle),
+      // _generateNavBarItem(title: 'DEBUG SIGN IN', icon: Icons.account_circle)
+    ];
+  }
+
+  List<Widget> generateScreens() {
+    return [
+      const ViewMessagesPage(),
+      const FriendPage(),
+      const TaskPage(),
+      const AccountPage(),
+      // const LogInPage(type: LogInType.signIn),
+    ];
   }
 }
