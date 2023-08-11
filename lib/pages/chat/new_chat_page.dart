107bbc35b67efa975198104cde7b75306cd1b488
sb2bg <sullivanbognar@gmail.com>
2023-08-10 22:47:35 -0500

    Convert to use LoadingState

--- lib/pages/chat/new_chat_page.dart
+++ lib/pages/chat/new_chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:age_sync/utils/loading_state.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -15,76 +16,46 @@ class NewChatPage extends StatefulWidget {
   State<NewChatPage> createState() => _NewChatPageState();
 }
 
-class _NewChatPageState extends State<NewChatPage> {
+class _NewChatPageState extends LoadingState<NewChatPage> {
   final _searchController = TextEditingController();
   final _friends = <Profile>[];
-  var _loading = true;
 
   @override
-  void initState() {
-    super.initState();
-    _loadFriends();
-  }
-
-  Future<void> _loadFriends() async {
-    try {
-      final me = await supabase.getCurrentUser();
-      final friends = await me.getFriends();
-
-      setState(() {
-        _friends.addAll(friends);
-      });
-    } on PostgrestException catch (error) {
-      context.showErrorSnackBar(message: error.message);
-    } catch (error) {
-      context.showErrorSnackBar(message: error.toString());
-    } finally {
-      if (mounted) {
-        setState(() {
-          _loading = false;
-        });
-      }
-    }
+  onInit() async {
+    final me = await supabase.getCurrentUser();
+    _friends.addAll(await me.getFriends());
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
-          child: TextField(
-            controller: _searchController,
-            decoration: InputDecoration(
-              hintText: "Search...",
-              prefixIcon: Icon(
-                Icons.search,
-                color: Colors.grey.shade600,
-                size: 20,
-              ),
+  Widget? get header => Padding(
+        padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+        child: TextField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            hintText: "Search...",
+            prefixIcon: Icon(
+              Icons.search,
+              color: Colors.grey.shade600,
+              size: 20,
             ),
           ),
         ),
-        Expanded(
-          child: _loading
-              ? preloader
-              : Center(
-                  child: ListView.separated(
-                      separatorBuilder: (context, index) => const Divider(),
-                      itemCount: _friends.length,
-                      itemBuilder: (context, index) {
-                        final friend = _friends[index];
+      );
 
-                        return FriendEntry(
-                          profile: friend,
-                        );
-                      }),
-                ),
-        ),
-      ],
-    ));
+  @override
+  Widget buildLoaded(BuildContext context) {
+    return Center(
+      child: ListView.separated(
+          separatorBuilder: (context, index) => const Divider(),
+          itemCount: _friends.length,
+          itemBuilder: (context, index) {
+            final friend = _friends[index];
+
+            return FriendEntry(
+              profile: friend,
+            );
+          }),
+    );
   }
 }
 
