a42d1de2eaaf2c2db750de1233b1a2bd28e43ef4
sb2bg <sullivanbognar@gmail.com>
2023-09-11 00:17:33 -0500

    Add spectate page for chat rooms

--- lib/pages/chat/chat_page.dart
+++ lib/pages/chat/chat_page.dart
@@ -3,12 +3,11 @@ import 'dart:async';
 import 'package:age_sync/pages/view_account_page.dart';
 import 'package:age_sync/utils/constants.dart';
 import 'package:age_sync/utils/loading_state.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:age_sync/widgets/chat_bubble.dart';
 import 'package:flutter/material.dart';
 
 import 'package:age_sync/utils/message.dart';
 import 'package:age_sync/utils/profile.dart';
-import 'package:timeago/timeago.dart';
 
 class ChatPage extends StatefulWidget {
   static const routeName = '/chat';
@@ -95,7 +94,7 @@ class _ChatPageState extends LoadingState<ChatPage> {
                           itemBuilder: (context, index) {
                             final message = messages[index];
 
-                            return _ChatBubble(
+                            return ChatBubble(
                               message: message,
                               profile: message.isMine ? _me : _other,
                             );
@@ -197,96 +196,5 @@ class _MessageBarState extends State<_MessageBar> {
     context.tryDatabaseAsync(() async {
       await Message.create(supabase.userId, text, widget.roomId);
     });
-  }
-}
-
-class _ChatBubble extends StatelessWidget {
-  const _ChatBubble({
-    required this.message,
-    required this.profile,
-  });
-
-  final Message message;
-  final Profile profile;
-
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> chatContents = [
-      if (!message.isMine)
-        GestureDetector(
-          onTap: () => context.pushNamed(ViewAccountPage.routeName,
-              arguments: profile.id),
-          child: CircleAvatar(
-            radius: 20,
-            backgroundImage: CachedNetworkImageProvider(profile.avatarUrl),
-          ),
-        ),
-      const SizedBox(width: 12),
-      Flexible(
-        child: Container(
-          padding: const EdgeInsets.symmetric(
-            vertical: 8,
-            horizontal: 12,
-          ),
-          decoration: BoxDecoration(
-            color: message.isMine ? Colors.blue[600] : Colors.grey[900],
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Text(message.content),
-        ),
-      ),
-      const SizedBox(width: 12),
-      Text(format(message.createdAt, locale: 'en_short'),
-          style: const TextStyle(color: Colors.grey)),
-      const SizedBox(width: 60),
-    ];
-
-    if (message.isMine) {
-      chatContents = chatContents.reversed.toList();
-    }
-
-    final myActions = [
-      ListTile(
-        leading: const Icon(Icons.delete),
-        title: const Text('Delete'),
-        onTap: () {
-          message.delete();
-          context.pop();
-        },
-      ),
-    ];
-
-    final otherActions = [
-      ListTile(
-        leading: const Icon(Icons.account_circle),
-        title: const Text('View profile'),
-        onTap: () {
-          context.pushNamed(ViewAccountPage.routeName, arguments: profile.id);
-        },
-      ),
-      ListTile(
-        leading: const Icon(Icons.report),
-        title: const Text('Report Message'),
-        onTap: () {
-          print('Report message ${message.id}'); // TODO: Report message
-          context.pop();
-          showReportThankYouDialog(context);
-        },
-      ),
-    ];
-
-    return GestureDetector(
-      onLongPress: () {
-        context.showMenu(message.isMine ? myActions : otherActions);
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 18),
-        child: Row(
-          mainAxisAlignment:
-              message.isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
-          children: chatContents,
-        ),
-      ),
-    );
   }
 }
---
+++ lib/pages/chat/spectate_room_page.dart
@@ -0,0 +1,78 @@
+import 'package:age_sync/utils/constants.dart';
+import 'package:age_sync/utils/loading_state.dart';
+import 'package:age_sync/utils/message.dart';
+import 'package:age_sync/utils/room.dart';
+import 'package:age_sync/widgets/chat_bubble.dart';
+import 'package:flutter/material.dart';
+
+class SpectateChatRoomPage extends StatefulWidget {
+  static const routeName = '/spectate';
+
+  const SpectateChatRoomPage({super.key, required this.roomId});
+
+  final String roomId;
+
+  @override
+  State<SpectateChatRoomPage> createState() => _SpectateChatRoomPageState();
+}
+
+class _SpectateChatRoomPageState extends LoadingState<SpectateChatRoomPage> {
+  late final RoomMeta _room;
+  late final Stream<List<Message>> _messagesStream;
+  late final user1 = _room.user1;
+  late final user2 = _room.user2;
+
+  @override
+  onInit() async {
+    _room = await RoomMeta.fromRoomId(widget.roomId);
+
+    _messagesStream = supabase
+        .from('messages')
+        .stream(primaryKey: ['id'])
+        .eq('room_id', widget.roomId)
+        .order('created_at')
+        .map((maps) => maps.map((map) => Message.fromMap(map: map)).toList());
+  }
+
+  @override
+  AppBar get loadingAppBar => AppBar();
+
+  @override
+  AppBar get loadedAppBar =>
+      AppBar(title: Text('${_room.user1.name} and ${_room.user2.name}'));
+
+  @override
+  bool get disableRefresh => true;
+
+  @override
+  Widget buildLoaded(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: StreamBuilder<List<Message>>(
+            stream: _messagesStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final messages = snapshot.data!;
+
+                return ListView.builder(
+                  itemCount: messages.length,
+                  itemBuilder: (context, index) {
+                    final message = messages[index];
+
+                    return ChatBubbleReceived(
+                      profile: message.profileId == user1.id ? user1 : user2,
+                      message: message,
+                    );
+                  },
+                );
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
