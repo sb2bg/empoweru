ad2f3557cad3f8e505f7d1bdcf3aa61fc4f50cfa
sb2bg <sullivanbognar@gmail.com>
2023-09-23 12:48:13 -0500

    Adjust settings

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,4 +1,5 @@
 import 'package:age_sync/pages/admin/admin_page.dart';
+import 'package:age_sync/pages/settings_page.dart';
 import 'package:age_sync/utils/loading_state.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
@@ -38,6 +39,39 @@ class _AccountPageState extends LoadingState<AccountPage> {
         title: const Text('Account'),
       );
 
+  changePfp() async {
+    final picker = ImagePicker();
+
+    final imageFile = await picker.pickImage(
+      source: ImageSource.gallery,
+      maxWidth: 300,
+      maxHeight: 300,
+    );
+
+    if (imageFile == null) {
+      return;
+    }
+
+    final bytes = await imageFile.readAsBytes();
+    final fileExt = imageFile.path.split('.').last;
+    final fileName = '${supabase.userId}.$fileExt';
+    final filePath = fileName;
+    await supabase.storage.from('avatars').uploadBinary(
+          filePath,
+          bytes,
+          fileOptions: FileOptions(contentType: imageFile.mimeType),
+        );
+    final imageUrl = await supabase.storage
+        .from('avatars')
+        .createSignedUrl(filePath, 60 * 60 * 24 * 365 * 10);
+
+    // TODO: this won't work with RLS
+    await supabase.from('profiles').upsert({
+      'id': supabase.userId,
+      'avatar_url': imageUrl,
+    });
+  }
+
   @override
   Widget buildLoaded(BuildContext context) {
     return Padding(
@@ -45,117 +79,90 @@ class _AccountPageState extends LoadingState<AccountPage> {
       child: Column(
         children: [
           ListTile(
-              leading: GestureDetector(
-                onTap: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                            title: const Text('Change avatar'),
-                            content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                ListTile(
-                                  leading: const Icon(Icons.camera_alt),
-                                  title: const Text('Take a photo'),
-                                  onTap: () => print('TODO'),
-                                ),
-                                ListTile(
-                                    leading: const Icon(Icons.photo),
-                                    title: const Text('Choose from gallery'),
-                                    onTap: () async {
-                                      final picker = ImagePicker();
-
-                                      final imageFile = await picker.pickImage(
-                                        source: ImageSource.gallery,
-                                        maxWidth: 300,
-                                        maxHeight: 300,
-                                      );
-
-                                      if (imageFile == null) {
-                                        return;
-                                      }
-
-                                      final bytes =
-                                          await imageFile.readAsBytes();
-                                      final fileExt =
-                                          imageFile.path.split('.').last;
-                                      final fileName =
-                                          '${supabase.userId}.$fileExt';
-                                      final filePath = fileName;
-                                      await supabase.storage
-                                          .from('avatars')
-                                          .uploadBinary(
-                                            filePath,
-                                            bytes,
-                                            fileOptions: FileOptions(
-                                                contentType:
-                                                    imageFile.mimeType),
-                                          );
-                                      final imageUrl = await supabase.storage
-                                          .from('avatars')
-                                          .createSignedUrl(filePath,
-                                              60 * 60 * 24 * 365 * 10);
-
-                                      // TODO: this won't work with RLS
-                                      await supabase.from('profiles').upsert({
-                                        'id': supabase.userId,
-                                        'avatar_url': imageUrl,
-                                      });
-                                    }),
-                                ListTile(
-                                    leading: const Icon(Icons.delete),
-                                    title: const Text('Remove avatar'),
-                                    onTap: () {
-                                      supabase.from('profiles').upsert({
-                                        'id': supabase.userId,
-                                        'avatar_url': defaultAvatarUrl,
-                                      }).then((value) => context.pop());
-                                    }),
-                                const Divider(),
-                                ListTile(
-                                  leading: const Icon(Icons.close),
-                                  title: const Text('Cancel'),
-                                  onTap: () => context.pop(),
-                                ),
-                              ],
-                            ),
-                          ));
-                },
-                child: CircleAvatar(
-                  radius: 20,
-                  backgroundImage:
-                      CachedNetworkImageProvider(_profile.avatarUrl),
-                ),
+            leading: GestureDetector(
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                          title: const Text('Change avatar'),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              ListTile(
+                                leading: const Icon(Icons.camera_alt),
+                                title: const Text('Take a photo'),
+                                onTap: () => print('TODO'),
+                              ),
+                              ListTile(
+                                  leading: const Icon(Icons.photo),
+                                  title: const Text('Choose from gallery'),
+                                  onTap: changePfp),
+                              ListTile(
+                                  leading: const Icon(Icons.delete),
+                                  title: const Text('Remove avatar'),
+                                  onTap: () {
+                                    supabase.from('profiles').upsert({
+                                      'id': supabase.userId,
+                                      'avatar_url': defaultAvatarUrl,
+                                    }).then((value) => context.pop());
+                                  }),
+                              const Divider(),
+                              ListTile(
+                                leading: const Icon(Icons.close),
+                                title: const Text('Cancel'),
+                                onTap: () => context.pop(),
+                              ),
+                            ],
+                          ),
+                        ));
+              },
+              child: CircleAvatar(
+                radius: 20,
+                backgroundImage: CachedNetworkImageProvider(_profile.avatarUrl),
               ),
-              title: Text(_profile.name),
-              trailing: IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () => print('TODO'),
-              )),
-          const Divider(),
-          if (_profile.admin)
-            Column(
+            ),
+            title: Text(_profile.name),
+            trailing: Wrap(
               children: [
-                ListTile(
-                  leading: const Icon(Icons.admin_panel_settings),
-                  title: const Text('Admin Panel'),
-                  onTap: () => context.pushNamed(AdminPage.routeName),
+                IconButton(
+                  icon: const Icon(Icons.settings),
+                  onPressed: () => context.pushNamed(SettingsPage.routeName),
                 ),
-                const Divider(),
+                if (_profile.admin)
+                  IconButton(
+                    icon: const Icon(Icons.admin_panel_settings),
+                    onPressed: () => context.pushNamed(AdminPage.routeName),
+                  ),
               ],
             ),
+          ),
+          const Divider(),
           ListTile(
-              leading: const Icon(Icons.email_outlined),
-              title: const Text('Change email'),
-              onTap: () => print('TODO')),
+            leading: const Icon(Icons.person_outline),
+            title: const Text('Edit Profile'),
+            onTap: () => print('TODO'),
+          ),
           ListTile(
-              leading: const Icon(Icons.lock_outline),
-              title: const Text('Change password'),
-              onTap: () => print('TODO')),
+            leading: const Icon(Icons.notifications_outlined),
+            title: const Text('Notification Settings'),
+            onTap: () => context.pushNamed(SettingsPage.routeName),
+          ),
           ListTile(
-              leading: const Icon(Icons.cake),
-              title: const Text('Change birthday'),
-              onTap: () => print('TODO')),
+            leading: const Icon(Icons.language_outlined),
+            title: const Text('Language'),
+            onTap: () => print('TODO'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.color_lens_outlined),
+            title: const Text('App Theme'),
+            onTap: () => print('TODO'),
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.feedback_outlined),
+            title: const Text('Feedback'),
+            onTap: () => print('TODO'),
+          ),
           const Divider(),
           ListTile(
             leading: const Icon(Icons.logout),
@@ -166,14 +173,6 @@ class _AccountPageState extends LoadingState<AccountPage> {
                   message: 'Are you sure you want to sign out?',
                   onConfirm: _signOut);
             },
-          ),
-          const Divider(),
-          ListTile(
-            leading: const Icon(
-              Icons.delete_outline,
-            ),
-            title: const Text('Delete account'),
-            onTap: () => print('TODO: Delete account and all associated data'),
           ),
         ],
       ),
