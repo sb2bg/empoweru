3921aae79324c6d7cdb243c5d3638e6ad7d1107d
sb2bg <sullivanbognar@gmail.com>
2023-09-25 01:53:00 -0500

    All of card opens alert

--- lib/widgets/task_view.dart
+++ lib/widgets/task_view.dart
@@ -21,6 +21,7 @@ class _TaskViewState extends State<TaskView> {
   void initState() {
     super.initState();
 
+    // TODO: cache names
     Profile.fromId(widget.task.ownerId).then((profile) {
       setState(() {
         _name = profile.name;
@@ -30,108 +31,142 @@ class _TaskViewState extends State<TaskView> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: GestureDetector(
-        onTap: () {
-          showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-              title: Flexible(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(widget.task.name,
-                        style: titleStyle, overflow: TextOverflow.ellipsis),
-                    Text(
-                      'By ${_name ?? 'Unknown'} ${DateFormat.yMMMMd().format(widget.task.createdAt)}',
-                      style: whiteMetaStyle,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    Text(
-                      'Due ${DateFormat.yMMMMd().format(widget.task.deadline)}',
-                      style: whiteMetaStyle,
-                    ),
-                  ],
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.task.name,
+                  style: titleStyle,
+                  overflow: TextOverflow.ellipsis,
                 ),
-              ),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(widget.task.details, style: subtitleStyle),
-                ],
-              ),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    taskController.toggleTask(widget.task);
-                    setState(() {});
-                    context.pop();
-                  },
-                  child: Text(widget.task.completed
-                      ? 'Mark Uncompleted'
-                      : 'Mark Completed'),
+                Text(
+                  'By ${_name ?? 'Unknown'} ${DateFormat.yMMMMd().format(widget.task.createdAt)}',
+                  style: whiteMetaStyle,
+                  overflow: TextOverflow.ellipsis,
                 ),
-                TextButton(
-                  onPressed: () {
-                    context.pop();
-                  },
-                  child: const Text('Close'),
+                Text(
+                  'Due ${DateFormat.yMMMMd().format(widget.task.deadline)}',
+                  style: whiteMetaStyle,
                 ),
               ],
             ),
-          );
-        },
-        child: ListTile(
-          title: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AnimatedCrossFade(
-                duration: const Duration(milliseconds: 250),
-                crossFadeState: widget.task.completed
-                    ? CrossFadeState.showSecond
-                    : CrossFadeState.showFirst,
-                firstChild: Expanded(
-                  child: Text(
-                    widget.task.name,
-                    style: subtitleStyle,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                secondChild: Expanded(
-                  child: Text(
-                    widget.task.name,
-                    style: subtitleStyle.copyWith(
-                        decoration: TextDecoration.lineThrough),
-                    overflow: TextOverflow.ellipsis,
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(widget.task.details, style: subtitleStyle),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  taskController.toggleTask(widget.task);
+                  setState(() {});
+                  context.pop();
+                },
+                child: Text(
+                  widget.task.completed ? 'Mark Incomplete' : 'Mark Complete',
+                  style: TextStyle(
+                    color: widget.task.completed
+                        ? Colors.redAccent
+                        : Colors.lightGreen,
                   ),
                 ),
               ),
-              Text(
-                'by ${_name ?? 'Unknown'} on ${DateFormat.yMMMMd().format(widget.task.createdAt)}',
-                style: metaStyle.copyWith(
-                  overflow: TextOverflow.ellipsis,
-                ),
+              TextButton(
+                onPressed: () {
+                  context.pop();
+                  context.pushNamed(
+                    '/task/${widget.task.id}',
+                    arguments: widget.task,
+                  );
+                },
+                child: const Text('Edit'),
               ),
-              Text(
-                  widget.task.completed
-                      ? 'Completed'
-                      : 'due ${DateFormat.yMMMMd().format(widget.task.deadline)} • repeats ${widget.task.repeat.name}',
-                  style: metaStyle,
-                  overflow: TextOverflow.ellipsis),
-              const SizedBox(height: 4),
+              TextButton(
+                onPressed: () {
+                  // taskController.deleteTask(widget.task);
+                  // setState(() {});
+                  context.pop();
+                },
+                child: const Text('Delete'),
+              ),
             ],
           ),
-          subtitle: Text(widget.task.details,
-              style: metaStyle.copyWith(color: Colors.white, fontSize: 14)),
-          trailing: Checkbox(
-            value: widget.task.completed,
-            onChanged: (value) {
-              taskController.toggleTask(widget.task);
-              setState(() {});
-            },
+        );
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Row(
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        child: AnimatedCrossFade(
+                          duration: const Duration(milliseconds: 250),
+                          crossFadeState: widget.task.completed
+                              ? CrossFadeState.showSecond
+                              : CrossFadeState.showFirst,
+                          firstChild: Text(
+                            widget.task.name,
+                            style: subtitleStyle,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          secondChild: Text(
+                            widget.task.name,
+                            style: subtitleStyle.copyWith(
+                                decoration: TextDecoration.lineThrough),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    'by ${_name ?? 'Unknown'} on ${DateFormat.yMMMMd().format(widget.task.createdAt)}',
+                    style: metaStyle.copyWith(
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Text(
+                      widget.task.completed
+                          ? 'Completed'
+                          : 'due ${DateFormat.yMMMMd().format(widget.task.deadline)} • repeats ${widget.task.repeat.name}',
+                      style: metaStyle,
+                      overflow: TextOverflow.ellipsis),
+                  const SizedBox(height: 4),
+                  Text(
+                    widget.task.details,
+                    style: metaStyle.copyWith(color: Colors.white),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              IconButton(
+                onPressed: () {
+                  taskController.toggleTask(widget.task);
+                  setState(() {});
+                },
+                icon: Icon(
+                  widget.task.completed
+                      ? Icons.check_box
+                      : Icons.check_box_outline_blank,
+                  color: widget.task.completed ? Colors.green : Colors.grey,
+                ),
+              ),
+            ],
           ),
         ),
       ),
