a03438bad754f9a4103561ee21bc9c280694c3cb
sb2bg <sullivanbognar@gmail.com>
2024-02-29 14:21:14 -0600

    Refactor message controller and add stream controllers

---
+++ lib/utils/stream_controllers.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'dart:collection';
+
+import 'package:age_sync/utils/chat/message.dart';
+import 'package:age_sync/utils/constants.dart';
+import 'package:age_sync/utils/room.dart';
+
+class StreamControllers {
+  late final Stream<HashMap<String, List<Message>>> messageStream;
+  late final Stream<List<Room>> roomStream;
+
+  StreamControllers() {
+    roomStream = supabase
+        .from('room_participants')
+        .stream(primaryKey: ['room_id'])
+        .eq('profile_id', supabase.userId)
+        .asyncMap((maps) async {
+          final List<Room> rooms = [];
+
+          for (final map in maps) {
+            rooms.add(await Room.fromId(map['room_id']));
+          }
+
+          return rooms;
+        });
+
+    messageStream = supabase
+        .from('messages')
+        .stream(primaryKey: ['id'])
+        .order('created_at')
+        .map((maps) {
+          final HashMap<String, List<Message>> messages = HashMap();
+
+          for (final map in maps) {
+            final message = Message.fromMap(map: map);
+
+            if (messages.containsKey(message.roomId)) {
+              messages[message.roomId]!.add(message);
+            } else {
+              messages[message.roomId] = [message];
+            }
+          }
+
+          return messages;
+        });
+  }
+}
