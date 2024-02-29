4261747bf3f3d89fd9e8a5a55378a26f7f45991c
sb2bg <sullivanbognar@gmail.com>
2024-02-29 14:20:45 -0600

    Add learning_page.dart

---
+++ lib/pages/learning_page.dart
@@ -0,0 +1,64 @@
+import 'package:age_sync/pages/beta_page.dart';
+import 'package:age_sync/utils/constants.dart';
+import 'package:age_sync/utils/loading_state.dart';
+import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+class OpportunityPage extends StatefulWidget {
+  const OpportunityPage({Key? key}) : super(key: key);
+
+  static const routeName = '/opportunity';
+  static const beta = true;
+
+  @override
+  State<OpportunityPage> createState() => _OpportunityPageState();
+}
+
+class _OpportunityPageState extends LoadingState<OpportunityPage> {
+  final YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: 'pbifXGYzYXU',
+    flags: const YoutubePlayerFlags(
+      autoPlay: false,
+    ),
+  );
+
+  @override
+  onInit() async {}
+
+  @override
+  AppBar get constAppBar => AppBar(
+        title: const Text('Connect'),
+      );
+
+  @override
+  Widget buildLoaded(BuildContext context) {
+    if (OpportunityPage.beta) {
+      return const BetaPage();
+    }
+
+    return Center(
+        child: Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Introduction to Houston Food Bank',
+            style: titleStyle,
+          ),
+          const SizedBox(height: 10),
+          YoutubePlayer(
+            controller: _controller,
+            showVideoProgressIndicator: true,
+            progressIndicatorColor: Colors.blueAccent,
+            progressColors: ProgressBarColors(
+              playedColor: Colors.blueAccent,
+              handleColor: Colors.blueAccent,
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
