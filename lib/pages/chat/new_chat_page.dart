dda8b69e86993b853d339be2ed4c7732d8e4f671
sb2bg <sullivanbognar@gmail.com>
2024-03-07 14:00:32 -0600

    Add search functionality to NewChatPage

--- lib/pages/chat/new_chat_page.dart
+++ lib/pages/chat/new_chat_page.dart
@@ -16,14 +16,34 @@ class NewChatPage extends StatefulWidget {
 class _NewChatPageState extends LoadingState<NewChatPage> {
   final _searchController = TextEditingController();
   final _friends = <Profile>[];
+  late List<Profile> _selected = [];
 
   @override
   bool get disableRefresh => true;
 
   @override
-  onInit() async {
+  Future<void> onInit() async {
     final me = await supabase.getCurrentUser();
     _friends.addAll(await me.getFriends());
+    _selected = _friends;
+  }
+
+  void search() {
+    final query = _searchController.text;
+
+    if (query.isEmpty) {
+      setState(() {
+        _selected = _friends;
+      });
+      return;
+    }
+
+    setState(() {
+      _selected = _friends
+          .where((friend) =>
+              friend.name.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
   }
 
   @override
@@ -31,12 +51,22 @@ class _NewChatPageState extends LoadingState<NewChatPage> {
         padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
         child: TextField(
           controller: _searchController,
+          onChanged: (value) {
+            search();
+          },
           decoration: InputDecoration(
             hintText: "Search...",
             prefixIcon: Icon(
               Icons.search,
               color: Colors.grey.shade600,
               size: 20,
+            ),
+            suffixIcon: IconButton(
+              icon: const Icon(Icons.clear),
+              onPressed: () {
+                _searchController.clear();
+                search();
+              },
             ),
           ),
         ),
@@ -47,7 +77,7 @@ class _NewChatPageState extends LoadingState<NewChatPage> {
     return Center(
       child: ListView.separated(
           separatorBuilder: (context, index) => const Divider(),
-          itemCount: _friends.length,
+          itemCount: _selected.length,
           itemBuilder: (context, index) {
             final friend = _friends[index];
 
@@ -77,7 +107,7 @@ class FriendEntry extends StatelessWidget {
           ),
           title: Text(profile.name),
           trailing: IconButton(
-            icon: const Icon(Icons.add),
+            icon: const Icon(Icons.arrow_forward),
             onPressed: () {
               context.pushNamed(ChatPage.routeName, arguments: profile);
             },
